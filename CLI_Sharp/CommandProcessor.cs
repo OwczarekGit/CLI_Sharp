b5f0558afb1bb9f3e32f2a27eebf6256259d391c
OwczarekGit <OwczarekGit>
2021-02-14 13:58:14 +0100

    CommandProcessor now has access to ConsoleDisplay and Logger instances.

--- CLI_Sharp/CommandProcessor.cs
+++ CLI_Sharp/CommandProcessor.cs
@@ -4,6 +4,18 @@ namespace CLI_Sharp
 {
     public abstract class CommandProcessor
     {
+        public ConsoleDisplay display { get; private set; }
+        public Logger logger { get; private set; }
+
+        public void setDisplay(ConsoleDisplay display)
+        {
+            this.display = display;
+        }
+
+        public void setLogger(Logger logger)
+        {
+            this.logger = logger;
+        }
         public abstract void processCommand(String cmd);
     }
 }
