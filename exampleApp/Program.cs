878c7b7826e730cc8e371a5a609ecbbb4c96a30c
OwczarekGit <OwczarekGit>
2021-02-16 04:39:31 +0100

    Added more comments to exampleApp.

--- exampleApp/Program.cs
+++ exampleApp/Program.cs
@@ -11,7 +11,8 @@ namespace exampleApp
         // Logger class provides methods for: warnings, errors, information, plain text.
         // You can implement your own Logger and add more methods or override the default ones.
         // It is a good idea to make Logger instance static so it can be used from other classes.
-        public static Logger logger = new Logger();
+        public static readonly Logger Logger = new Logger();
+        // You can pass "true" in the constructor to enable logging to file. 
         
         static void Main(string[] args)
         {
@@ -22,17 +23,19 @@ namespace exampleApp
             MyProcessor processor = new MyProcessor(); 
             
             // Creating ConsoleDisplay instance.
-            ConsoleDisplay display = new ConsoleDisplay(logger, processor); 
+            ConsoleDisplay display = new ConsoleDisplay(Logger, processor);
             
             // Starting the CLI.
             display.start(); 
             
             // Your program continues as usual.
+            // !IMPORTANT! You must keep the program running as CLI_Sharp runs on separate thread
+            // therefore when main thread of your program ends the program WILL exit.
             // In this case i'm just gonna make a while loop that uses Logger instance
             // to display some warnings every second.
             while (true)
             {
-                logger.warn("This is a warning.");
+                Logger.warn("This is a warning.");
                 Thread.Sleep(1000);
             }
         }
